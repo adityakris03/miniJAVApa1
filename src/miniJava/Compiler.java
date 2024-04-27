70f3335b063e63510bb026afa977914f83b09f36
Aditya Krishna <51248874+adityakris03@users.noreply.github.com>
2024-04-27 19:01:03 -0400

    removed print statement

--- src/miniJava/Compiler.java
+++ src/miniJava/Compiler.java
@@ -51,7 +51,7 @@ public class Compiler {
                     CodeGenerator cg = new CodeGenerator(errors);
                     cg.parse(p);
                     if (errors.hasErrors()) errors.outputErrors();
-                    else System.out.println("making a.out");
+                    //else System.out.println("making a.out");
                 }
             }
         } catch (FileNotFoundException e) {
